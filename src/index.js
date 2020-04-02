6d998c9e71cd39f54a64f347bf070c18ded48e00
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-04-02 20:29:00 +0100

    add 'ERROR' token in case of bad input

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 function buildLexer(tokens) {
   const SKIP_TYPE = 'SPACE';
+  const ERROR_TYPE = 'ERROR';
   const tokenNames = tokens.map((t) => t[0]);
   const tokenRegs = tokens.map((t) => t[1]);
 
@@ -25,14 +26,21 @@ function buildLexer(tokens) {
   const lexer = (str) => {
     let match;
     const result = [];
+    let index = 0; // Using index to determine if we got an error.
     while (match = regexp.exec(str)) {
       const t = getToken(match.groups);
+      const type = t;
+      const value = match.groups[t];
+      // match.index is where it started matching
+      index = match.index + value.length;
       if (t === SKIP_TYPE) {
         continue;
       }
-      const type = t;
-      const value = match.groups[t];
       result.push(tokenize(type, value));
+    }
+    if (index !== str.length) {
+      const value = str.slice(index, index + 10);
+      result.push(tokenize(ERROR_TYPE, value));
     }
     return result;
   };
